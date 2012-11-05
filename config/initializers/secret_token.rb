519c69a5a17cb17c1f417a2cd9c31533e982ea89
Venkat <venkat@venkat-Inspiron-N5050.(none)>
2012-11-05 13:31:08 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JobsApp::Application.config.secret_token = 'ab3ac821bd14c034607fd1bfe2b94608f6f419bd30d4b32545bd01a3be93c2619f6e8ea03c76c6b7519bd5063ecf0b70a4399436aecbc8ee9ef910e047d8b64d'
